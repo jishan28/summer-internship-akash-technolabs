e41922415fcdc9a054653a53557635da8504c68a
jishan28 <98313000+jishan28@users.noreply.github.com>
2022-07-05 09:40:06 +0530

    Update insert.py

--- CRUD task/insert.py
+++ CRUD task/insert.py
@@ -11,12 +11,12 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 sql = "INSERT INTO tbl_user(user_id,user_firstname,user_lastname,user_address,user_email,user_password) VALUES('001','jishan','mahida','gujarat','jishanmahida880@gmail.com','280901')"
-sql1 = "INSERT INTO tbl_user(user_id,user_firstname,user_lastname,user_address,user_email,user_password) VALUES('002','shan','xyz','delhi','shanmahida880@gmail.com','280901')"
-sql2 = "INSERT INTO tbl_user(user_id,user_firstname,user_lastname,user_address,user_email,user_password) VALUES('003','ishan','abc','goa','ishanmahida880@gmail.com','280901')"
+sql = "INSERT INTO tbl_user(user_id,user_firstname,user_lastname,user_address,user_email,user_password) VALUES('002','shan','xyz','delhi','shanmahida880@gmail.com','280901')"
+sql = "INSERT INTO tbl_user(user_id,user_firstname,user_lastname,user_address,user_email,user_password) VALUES('003','ishan','abc','goa','ishanmahida880@gmail.com','280901')"
 
 mycursor.execute(sql)
-mycursor.execute(sql1)
-mycursor.execute(sql2)
+mycursor.execute(sql)
+mycursor.execute(sql)
 
 mydb.commit()
 
